a328655b018921f7264d78e1e7d2d9f50cbe795a
Анатолий <volotovskiy111@gmail.com>
2020-02-15 13:02:14 +0300

    module3-task2

--- js/main.js
+++ js/main.js
@@ -10,6 +10,10 @@ var TIMES = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var DESCRIPTION = ['Ооочень круто', 'Идеально просто', 'Космос, а не вариант', 'Маме бы понравилось'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var MIN_PRICE = 1000;
+var MAX_PRICE = 4000;
+var MIN_Y = 130;
+var MAX_Y = 630;
 
 var userDialog = document.querySelector('.map');
 userDialog.classList.remove('map--faded');
@@ -18,7 +22,7 @@ var data = [];
 
 var func = function () {
   var randomX = getRandom(0, 1001);
-  var randomY = getRandom(130, 631);
+  var randomY = getRandom(MIN_Y, MAX_Y + 1);
   var check = getRandomElement(TIMES);
   return {
     "author": {
@@ -27,7 +31,7 @@ var func = function () {
     "offer": {
       "title": getRandomElement(TITLE),
       "address": randomX + randomY,
-      "price": getRandom(1000, 4001),
+      "price": getRandom(MIN_PRICE, MAX_PRICE + 1),
       "type": getRandomElement(TYPE),
       "rooms": getRandomElement(ROOMS),
       "guests": getRandomElement(GUESTS),
